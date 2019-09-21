9d6e87268f8576db8a933c7a7f717e9ec029d62a
sathyaBabu <sathyhelp123@gmail.com>
2019-09-22 00:33:46 +0530

    Initial commit

---
+++ app/src/main/java/broadcast/service/com/serviceupdatingui/MainActivity.java
@@ -0,0 +1,63 @@
+package broadcast.service.com.serviceupdatingui;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "BroadcastTest";
+    private Intent intent;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intent = new Intent(this, BroadcastService.class);
+
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateUI(intent);
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startService(intent);
+        registerReceiver(broadcastReceiver, new IntentFilter(BroadcastService.BROADCAST_ACTION));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcastReceiver);
+        stopService(intent);
+    }
+
+    private void updateUI(Intent intent) {
+        String counter = intent.getStringExtra("counter");
+        String time    = intent.getStringExtra("time");
+        Log.d(TAG, counter);
+        Log.d(TAG, time);
+
+        TextView txtDateTime = (TextView) findViewById(R.id.txtDateTime);
+        TextView txtCounter = (TextView) findViewById(R.id.txtCounter);
+
+        txtDateTime.setText("Date    : "+time);
+         txtCounter.setText("Counter : "+counter);
+    }
+}
